2a8ee10f9578d1d582113c4c4dd95d424b20df19
NagiPragalathan <kk2069037@gmail.com>
2023-01-21 21:54:08 +0530

    images updated in home page

--- base/views.py
+++ base/views.py
@@ -5,10 +5,12 @@ import time
 from agora_token_builder import RtcTokenBuilder
 from .models import RoomMember
 import json
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from .models import Faculty_details, Users
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+import datetime
 
 
 # Create your views here.
@@ -23,7 +25,7 @@ def login_into_home(request):
     print(user)
     if user is not None:
         login(request, user)
-        user_detials = Users.objects.get(user_name = user_name)
+        user_detials = Users.objects.get(mail_id = user_name)
         role = user_detials.role
         usr_name = user_detials.user_name
         if role == 3 :
@@ -35,11 +37,54 @@ def login_into_home(request):
     else:
         return render(request,"login/login.html")
 
+#--------------------------
+def Personal_detials(request):
+    usr_id = request.user.id
+    usr_obj = User.objects.get(id=usr_id)
+    faculty_details = Faculty_details.objects.get(mail=usr_obj.username)
+    # request and get datas ..............
+    role = Users.objects.get(mail_id = usr_obj.username)
+    id_number = request.POST.get('idcard')
+    name1 = request.POST.get('F_name')
+    name2 = request.POST.get('surname')
+    name = name1+' '+name2
+    designation = request.POST.get('designation')
+    department = request.POST.get('department')
+    experience = request.POST.get('experience')
+    qualififcation = request.POST.get('qualififcation')
+    assessment_period = request.POST.get('AP')
+    date_of_join = request.POST.get('date')
+    bio = request.POST.get('about')
+    d = date_of_join.split("-")
+    date_formate = datetime.date(int(d[0]), int(d[1]), int(d[2]))
+    my_uploaded_file = request.FILES['file_upload']
+    print(date_of_join)
+    usr_id = request.user.id
+    usr_obj = User.objects.get(id=usr_id)
+    edit = Faculty_details.objects.get(mail=usr_obj.username)
+    print(edit.mail)
+    edit.role=role
+    edit.id_number=id_number
+    edit.designation=designation
+    edit.department=department
+    edit.experience=experience
+    edit.qualififcation=qualififcation
+    edit.assessment_period=assessment_period
+    edit.date_of_join=date_formate
+    edit.image = my_uploaded_file
+    edit.bio=bio
+    edit.save()
+    return render(request,"home/index.html",{'user_name':usr_obj.username,'detials':faculty_details})
+
 
 #-------------------------
-
+@login_required()
 def home(request):
-    return render(request,"home/index.html")
+    usr_id = request.user.id
+    usr_obj = User.objects.get(id=usr_id)
+    name = Users.objects.get(mail_id=usr_obj.username)
+    faculty_details = Faculty_details.objects.get(user_name=name.user_name)
+    return render(request,"home/index.html",{'user_name':usr_obj.username,'detials':faculty_details})
     
 
 def add_faculty(request):
@@ -57,14 +102,17 @@ def add_usr(request):
     facultys = Faculty_details.objects.all()
     for i in facultys:
         print(i.name)
-        
-    add_user = Users(user_name=usr_name,mail_id=mail,password=password,role=role)
-    add_user.save()
-    current_user = Users.objects.get(mail_id=mail)
-    Fac_del = Faculty_details(user_name=usr_name,role=current_user, id_number=0, name=add_user.user_name)
-    Fac_del.save()
-    user = User.objects.create_user(usr_name, mail, password)
-    user.save()
+    
+    try:
+        add_user = Users(user_name=usr_name,mail_id=mail,password=password,role=role)
+        add_user.save()
+        current_user = Users.objects.get(mail_id=mail)
+        Fac_del = Faculty_details(user_name=usr_name,mail=mail,role=current_user, id_number=0, name=add_user.user_name)
+        Fac_del.save()
+        user = User.objects.create_user(mail, usr_name, password)
+        user.save()
+    except:
+        print("unique are missed....")
     return render(request,"admin/Admin_page_to_add_Facuilty.html",{'users':facultys})
 
 def add_facu(request):
