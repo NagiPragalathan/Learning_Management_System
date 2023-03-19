6a86e1139139c17107e74b17563a507a0203fcda
NagiPragalathan <kk2069037@gmail.com>
2023-03-19 20:31:51 +0530

    classroom are modifyed with the error of teacher template(login template)

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import pyautogui
+import random
+import time
+sorry = ['sorry deeeeeeeee', 'sorry de eni epade panna matan',
+         'un kal le kudhe vilere', 'So sorryy', 'Sorry diiii']
+time.sleep(2)
 
-# Create your tests here.
+
+for i in range(100):
+    if random.choice([1]) == 1:
+        pyautogui.typewrite(random.choice(sorry))
+        pyautogui.press('enter')
+        time.sleep(1)
+    else:
+        pyautogui.typewrite(
+            "sorry d"+random.choice(["e", 'i'])*(random.choice([i for i in range(10)])))
+        pyautogui.press('enter')
+        time.sleep(1)
