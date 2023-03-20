e7c9545d4c546352d06a60940de77ce54416d4ef
NagiPragalathan <kk2069037@gmail.com>
2023-03-21 04:49:56 +0530

    hosting encouter

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,11 @@
 
 echo " Build start ............." 
 
+yum install sqlite-devel
+./configure
+make && make altinstall
 pip install --upgrade pip
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
-
+python3.9 manage.py collectstatic --noinput --clear
+ 
 echo "build end/................."
