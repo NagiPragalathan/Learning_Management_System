c26d3bfbf8091295a7898b4017a696ee0cc49bc0
NagiPragalathan <kk2069037@gmail.com>
2023-03-21 05:25:05 +0530

    altered the buil_files

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,7 @@ echo " Build start ............."
 
 pip install --upgrade pip
 pip install -r requirements.txt
+pip install django
 python3.9 manage.py collectstatic --noinput --clear
  
 echo "build end/................."
