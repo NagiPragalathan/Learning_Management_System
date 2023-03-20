50b73fec24ab052e708e55eb4164721d288af58a
NagiPragalathan <kk2069037@gmail.com>
2023-03-20 22:11:46 +0530

    hosting learn

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,8 @@
 # build_files.sh
+echo " Build start ............." 
+
+pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+
+echo "build end/................."
