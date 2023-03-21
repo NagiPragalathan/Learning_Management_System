04e464c2bfa2d48862d502ccdf957198a368cbd0
NagiPragalathan <kk2069037@gmail.com>
2023-03-21 05:34:44 +0530

    solveing...

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,8 @@ echo " Build start ............."
 pip install --upgrade pip
 pip install -r requirements.txt
 pip install django
+pip install django-widget-tweaks==1.4.8
+pip install django-widget-tweaks
 python3.9 manage.py collectstatic --noinput --clear
  
 echo "build end/................."
