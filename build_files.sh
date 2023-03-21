61324b54020f788776296b561619c0460d39f96d
NagiPragalathan <kk2069037@gmail.com>
2023-03-21 06:20:06 +0530

    migrate

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,7 @@ echo " Build start ............."
 pip install -r requirements.txt
 pip install django
 pip install django-widget-tweaks
+python manage.py migrate
 python3.9 manage.py collectstatic --noinput --clear
  
 echo "build end/................."
