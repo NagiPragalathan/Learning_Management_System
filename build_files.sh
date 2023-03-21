036bd77c6d3110075f7eec121704049a17602558
NagiPragalathan <kk2069037@gmail.com>
2023-03-21 05:44:06 +0530

    some commentes are removed

--- build_files.sh
+++ build_files.sh
@@ -2,13 +2,10 @@
 
 echo " Build start ............." 
 
-pip install --upgrade pip
-sudo apt-get install libpq-dev
 pip install psycopg2-binary
 pip install -r requirements.txt
 pip install psycopg2
 pip install django
-pip install django-widget-tweaks==1.4.8
 pip install django-widget-tweaks
 python3.9 manage.py collectstatic --noinput --clear
  
